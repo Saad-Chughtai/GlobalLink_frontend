9c46f4e36e28f7ad4b7ada6571295e2c00a3b0c9
Saad-Chughtai <saad.habib393@gmail.com>
2025-12-18 00:13:01 +0500

    handel responsiveness

--- src/components/home/Testimonials.jsx
+++ src/components/home/Testimonials.jsx
@@ -18,14 +18,53 @@ const Testimonials = () => {
     autoplay: true,
     autoplaySpeed: 5000,
     pauseOnHover: true,
+    swipe: true,
+    touchMove: true,
+    arrows: true,
     responsive: [
       {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: true,
+        },
+      },
+      {
         breakpoint: 1024,
-        settings: { slidesToShow: 2 },
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: true,
+        },
       },
       {
         breakpoint: 640,
-        settings: { slidesToShow: 1 },
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: true,
+          centerMode: false,
+        },
       },
     ],
   };
